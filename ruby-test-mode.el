0d3b1c9d0487ba2944319fa9d409e654583f0114
r0man <roman@burningswell.com>
2017-10-03 18:10:08 +0200

    Add standard license

--- ruby-test-mode.el
+++ ruby-test-mode.el
@@ -12,7 +12,20 @@
 ;; Keywords: ruby unit test rspec
 ;; Package-Requires: ((ruby-mode "1.0") (pcre2el "1.8"))
 
-;; This software can be redistributed. GPL v3 applies.
+;; This file is not part of GNU Emacs.
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
