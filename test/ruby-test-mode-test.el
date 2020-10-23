5ace23c178ed8b565627823f0e21129fefc13785
Aaron Madlon-Kay <aaron@madlon-kay.com>
2020-10-23 21:54:35 +0900

    Move PAGER envar to customizable variable

--- test/ruby-test-mode-test.el
+++ test/ruby-test-mode-test.el
@@ -84,7 +84,7 @@
     (should (string-match "bundle exec ruby -I'lib:test' -rrubygems ./test/unit_test.rb"
               (ruby-test-command "./test/unit_test.rb"))))
   (with-test-file "config/environment.rb"
-    (should (string-match "PAGER=cat bundle exec rails test -v ./test/unit_test.rb"
+    (should (string-match "bundle exec rails test -v ./test/unit_test.rb"
               (ruby-test-command "./test/unit_test.rb"))))
   (should (string-match "bundle exec rspec -b ./spec/unit_spec.rb"
             (ruby-test-spec-command "./spec/unit_spec.rb"))))
